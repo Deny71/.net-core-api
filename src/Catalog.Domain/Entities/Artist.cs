0d9b3f269429d1d07e5fb29e773b53fb7b60942b
Deny71 <damian-pecko@wp.pl>
2021-02-02 22:29:09 +0100

    FC

---
+++ src/Catalog.Domain/Entities/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Catalog.Domain.Entities
+{
+    public class Artist
+    {
+        public Guid ArtistId { get; set; }
+        public string ArtistName { get; set; }
+        public ICollection<Item> Items { get; set; }
+    }
+}
